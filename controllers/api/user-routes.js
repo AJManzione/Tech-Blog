a4107e70ca80789c8515853d42c83c397c5e2687
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-04 16:38:35 -0600

    added user-routes get and post methods

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    User,
+    Post,
+    Comment
+} = require('../../models')
+
+router.get('/', (req, res) => {
+    
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+router.post('/logout', (req, res) => {
+
+})
+
+module.exports = router;
