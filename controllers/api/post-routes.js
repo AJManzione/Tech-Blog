fcb2ecbf46927fbeb296f43f9ca97c45dc4bf64c
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-06 12:11:24 -0700

    create and delete post path's created

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,6 +5,8 @@ const {
     Comment
 } = require('../../models')
 
+
+// Get all the posts
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: ['id', 'content', 'title'],
@@ -28,15 +30,41 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-
-})
+    Post.create({
+        title: req.body.title,
+        content: req.body.post_content,
+        user_id: req.session.user_id
+    })
+    .then((data) => res.json(data))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.put('/:id', (req, res) => {
 
 })
 
 router.delete('/:id', (req, res) => {
-
+    Post.destroy({
+        where: {
+            id: req.params.id
+        },
+    })
+    .then((data) => {
+        if (!data) {
+            res.status(404).json({
+                message: 'This post could not be found'
+            });
+            return;
+        }
+        res.json(data);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 })
 
 module.exports = router;
