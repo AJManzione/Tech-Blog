8da34c7cd53fa482c57704412e0469c8caa33766
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-06 11:13:58 -0700

    create a user and find all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,18 +11,39 @@ router.get('/', (req, res) => {
             exclude: ['password']
         }
     })
-})
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.get('/:id', (req, res) => {
 
 })
 
 router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(data => {
+        req.session.save(() => {
+            req.session.user_id = data.id;
+            req.session.username = data.username;
+            req.session.loggedIn = true;
 
+            res.json(data);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
 
 router.post('/login', (req, res) => {
-
+    
 })
 
 router.post('/logout', (req, res) => {
