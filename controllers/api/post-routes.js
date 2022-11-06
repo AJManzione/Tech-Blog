1e8284c3ef063a0392ef72a25b3cbeb3bc1d3c84
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-06 14:21:59 -0700

    create, and update a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -33,12 +33,42 @@ router.get('/', (req, res) => {
 
 // get a single post 
 router.get('/:id', (req, res) => {
-
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'content', 'title'],
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_body', 'post_id', 'user_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }]
+    })
+    .then((data) => {
+        if (!data) {
+            res.status(404).json({
+                message: 'no posts found'
+            });
+            return;
+        }
+        res.json(data);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
 
 
 // create a single post
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         content: req.body.post_content,
