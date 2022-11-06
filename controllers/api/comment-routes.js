eafb72abf490ef1cad0c1273b6bfba7cf3ce57c4
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-06 12:14:46 -0700

    creating comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -5,17 +5,23 @@ const {
     Comment
 } = require('../../models');
 
+// Get all comments
 router.get('/', (req, res) => {
     Comment.findAll()
 })
 
-
+// Create a comment
 router.post('/', (req, res) => {
     Comment.create({
         comment_body: req.body.comment_body,
         post_id: req.body.post_id,
         user_id: req.body.user_id
     })
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 })
 
 
