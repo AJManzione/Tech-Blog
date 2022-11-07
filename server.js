05cebc570015642792fc56f2bd4e10454327493d
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-06 21:07:41 -0700

    testing depl

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-  secret: process.env.DB_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-      db: sequelize,
-      checkExpirationInterval: 1000 * 60 * 10,
-      expiration: 1000 * 60 * 30 
-  })
-};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,7 +13,6 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
