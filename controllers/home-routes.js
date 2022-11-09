781a50eb55bcc3a12731e2873943eaab29a69081
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-08 21:56:03 -0700

    re-working comment card

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,102 +6,116 @@ const {
     Comment
 } = require('../models');
 
+
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['id', 'title', 'content'],
-        include: [{
-            model: Comment,
-            attributes: ['id', 'comment_body', 'post_id', 'user_id'],
-            include: {
-                model: User,
-                attributes: ['username']
-            }
-        }]
-    })
-    .then(data => {
-        const posts = data.map(post => post.get({
-            plain: true
-        }));
-
-        res.render('homepage', {
-            posts,
-            loggedIn: req.session.loggedIn
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'created_at'
-        ],
-        include: [{
-                model: Comment,
-                attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
-                include: {
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
                     model: User,
                     attributes: ['username']
                 }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(data => {
-        if (!data) {
-            res.status(404).json({
-                message: 'No post found with this id'
+            ]
+        })
+        .then(data => {
+            const posts = data.map(post => post.get({
+                plain: true
+            }));
+
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
             });
-            return;
-        }
-
-        const post = data.get({
-            plain: true
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
         });
-
-        res.render('single-post', {
-            post,
-            loggedIn: req.session.loggedIn
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
 });
 
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(data => {
+            if (!data) {
+                res.status(404).json({
+                    message: 'No post found with this id'
+                });
+                return;
+            }
+
+            const post = data.get({
+                plain: true
+            });
+
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
         return;
     }
+
     res.render('login');
 });
 
 router.get('/signup', (req, res) => {
-    if(req.session.loggedIn) {
+    if (req.session.loggedIn) {
         res.redirect('/');
         return;
     }
+
     res.render('signup');
 });
 
+
 router.get('*', (req, res) => {
-    res.status(404).send('404, bad request!');
-});
+    res.status(404).send("Can't go there!");
+    // res.redirect('/');
+})
+
 
 module.exports = router;
