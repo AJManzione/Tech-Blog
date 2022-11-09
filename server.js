5acae6333f63e4ac9d8368eacb605ede43ec1439
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-08 21:32:32 -0700

    re-working date time

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const hbs = exphbs.create({
     helpers
 });
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'super secret secret',
+    secret: process.env.DB_SECRET,
     cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 10, 
+        checkExpirationInterval: 1000 * 60 * 10,
         expiration: 1000 * 60 * 30 
     })
 };
