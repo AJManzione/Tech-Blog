a1ec7642f9b6ef666f3f133eeb09cd75b68373bd
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-08 23:38:46 -0700

    de-bugging loggin

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -61,7 +61,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     User.create({
             username: req.body.username,
-            password: req.body.password
+            password: req.body.password.toString()
         })
         .then(dbUserData => {
             req.session.save(() => {
@@ -103,7 +103,7 @@ router.post('/login', (req, res) => {
                 });
             });
 
-            const validPassword = dbUserData.checkPassword(req.body.password);
+            const validPassword = dbUserData.checkPassword(req.body.password.toString());
 
             if (!validPassword) {
                 res.status(400).json({
